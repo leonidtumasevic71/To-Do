e0e5b24bf499b0c589894f9122bc0c604a0f5365
leonidtumasevic71 <leonidtumasevic71@gmail.com>
2026-04-13 17:47:05 +0300

    add modal

--- frontend/src/components/AddTaskModal.jsx
+++ frontend/src/components/AddTaskModal.jsx
@@ -1,28 +1,17 @@
 import './AddTaskModal.css';
 
-function AddTaskModal({isOpen, onClose, title, children})
+function AddTaskModal({isOpen, onClose, children})
 {
     if (!isOpen) return null;
 
     return (
-            <div className="modal-overlay" onClick={onClose}>
-                <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                    <div className="modal-header">
-                        <h2>{title}</h2>
-                        <button className="modal-close" onClick={onClose}>
-                            ✕
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        {children}
-                    </div>
-                    <div className="modal-footer">
-                        <button onClick={onClose}>Отмена</button>
-                        <button onClick={onClose}>Подтвердить</button>
-                    </div>
-                </div>
+        <div className='modal-overlay' onClick={onClose}>
+            <div className='modal-content' onClick={(e) => e.stopPropagation()}>
+                <button className="close-btn" onClick={onClose}>✕</button>
+                {children}
             </div>
-        )
+        </div>
+        );
 }
 
 export default AddTaskModal
